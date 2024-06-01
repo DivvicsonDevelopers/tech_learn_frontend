beaa12235e664d58c42e43289c840f258dc5afac
unknown <noblefarz7@gmail.com>
2024-06-01 11:17:31 -0700

    ui changes

--- src/pages/landing/componnts/ImagePage.jsx
+++ src/pages/landing/componnts/ImagePage.jsx
@@ -1,7 +1,6 @@
-import { Box, Flex, Image} from '@chakra-ui/react'
+import { Box, Flex, Image } from "@chakra-ui/react";
 import ImageLeft from "../../../assets/imageLeft.png";
-import ImageRight from "../../../assets/imageRight.png"
-
+import ImageRight from "../../../assets/imageRight.png";
 
 const ImagePage = () => {
   return (
@@ -10,38 +9,16 @@ const ImagePage = () => {
       my="20px"
       p={["20px 30px", "20px 30px", "20px 30px", "20px 150px"]}
     >
-      <Flex
-        flexDir={["column", "column", "column", "row"]}
-        alignItems={"center"}
-        position={"relative"}
-        gap={["40px", "40px", "40px", "0px"]}
-        width={"100%"}
-        height={["fit-content", "fit-content", "fit-content", "500px"]}
-        mt={["20px", "20px", "20px", "70px"]}
-        justifyContent={"center"}
-      >
-        <Box
-          position={["static", "static", "static", "absolute"]}
-          right={["0px", "0px", "0px", "600px"]}
-        >
-          <Image
-            src={ImageLeft}
-            height={["300px", "400px", "400px", "400px"]}
-          />
+      <Flex justify={"center"} gap={"50px"} flexDir={["column","column","column","row"]}>
+        <Box>
+          <Image src={ImageLeft} />
         </Box>
-        <Box
-          left={"600px"}
-          position={["static", "static", "static", "absolute"]}
-          bottom={"150px"}
-        >
-          <Image
-            src={ImageRight}
-            height={["300px", "400px", "400px", "400px"]}
-          />
+        <Box>
+          <Image src={ImageRight} />
         </Box>
       </Flex>
     </Box>
   );
-}
+};
 
-export default ImagePage
+export default ImagePage;
